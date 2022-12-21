1d383e406978a64e7c74014e05b00358fe7b0951
suraj <suraj34and94@gmail.com>
2022-12-21 19:09:26 +0530

    new route

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express=require('express')
 const app=express();
 
 const port=process.env.PORT || 3000
-app.get("/",(erq,res,next)=>{
+app.get("/home",(req,res,next)=>{
     res.send("Hello world")
 })
 app.listen(port,()=>{
