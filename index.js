da54cfbaa84f7506f1154f19b0f7eb55ac5b9e87
suraj <suraj34and94@gmail.com>
2022-12-21 17:46:17 +0530

    jk

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express=require('express')
 const app=express();
+
+const port=process.env.PORT || 3000
 app.get("/",(erq,res,next)=>{
     res.send("Hello world")
 })
-app.listen(3000,()=>{
+app.listen(port,()=>{
     console.log("listening on 3000")
 });
