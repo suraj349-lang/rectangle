2634b9e7cd62c4018ad00d5cf4d0d68233981009
suraj <suraj34and94@gmail.com>
2022-12-30 19:43:47 +0530

    cors

--- index.js
+++ index.js
@@ -3,5 +3,11 @@ const app=express();
 const feedRoutes=require("./routes/feed")
 
 const port=process.env.PORT || 3000
+app.use((req,res,next)=>{
+    res.setHeader("Access-Control-Allow-Origin","*")
+    res.setHeader("Access-Control-Allow-Methods",'GET,POST,PUT,PATCH,DELETE')
+    res.setHeader("Access-Control-Allow-Headers",'Content-Type,Authorization')
+    next();
+})
 app.use("/feed",feedRoutes)
 app.listen(port);
